6a476f7466ded245be56daf89b43a2d0a626d5f8
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-01-15 15:23:14 -0700

    Created Rocket class

---
+++ src/Rocket.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+
+public class Rocket implements SpaceShip {
+
+    private double cost;
+    private int weight;
+    private int cargoWeight;
+    private int maxWeight;
+    private double explosionChance;
+    private double crashChance;
+
+    private ArrayList<Item> cargo;
+
+    Rocket() {
+        cargo = new ArrayList<Item>();
+    }
+
+    // Getters and Setters
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getCargoWeight() {
+        return this.cargoWeight;
+    }
+
+    public int getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(int maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public double getExplosionChance() {
+        return explosionChance;
+    }
+
+    public void setExplosionChance(double explosionChance) {
+        this.explosionChance = explosionChance;
+    }
+
+    public double getCrashChance() {
+        return crashChance;
+    }
+
+    public void setCrashChance(double crashChance) {
+        this.crashChance = crashChance;
+    }
+
+    // SpaceShip Interface implementation
+    @Override
+    public boolean launch() {
+        return true;
+    }
+
+    @Override
+    public boolean land() {
+        return true;
+    }
+
+    @Override
+    public boolean canCarry(Item item) {
+        int w = this.weight + this.cargoWeight + item.weight;
+        if (w > this.maxWeight) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void carry(Item item) {
+        cargo.add(item);
+        cargoWeight += item.weight;
+    }
+}
