38497bf521150ba6ec23662bd90b76215f111ae3
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-01-15 15:44:28 -0700

    Created Simulation class

---
+++ src/Simulation.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Simulation {
+
+    public ArrayList<Item> loadItems(String filename) {
+        return new ArrayList<Item>();
+    }
+
+    public void loadU1(ArrayList<Item> items) {
+
+    }
+
+    public void loadU2(ArrayList<Item> items) {
+
+    }
+
+    public double runSimulation(ArrayList<Rocket> rockets) {
+        
+        return 0;
+    }
+}
