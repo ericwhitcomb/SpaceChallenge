7cb7c72b36c9c2e39f07fe49b5662948bbd848f9
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-01-15 16:16:56 -0700

    Finished code in Simulation class loadItems()

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,30 @@
 public class Item {
-    String name;
-    int weight;
+    private String name;
+    private int weight;
+
+    Item() {
+        name = "";
+        weight = 0;
+    }
+
+    Item(String name, int weight) {
+        this.setName(name);
+        this.setWeight(weight);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,13 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Simulation {
 
-    public ArrayList<Item> loadItems(String filename) {
-        return new ArrayList<Item>();
+    public ArrayList<Item> loadItems(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+
+        ArrayList<Item> items = new ArrayList<Item>();
+        String[] elements = null;
+        while (scanner.hasNextLine()) {
+            elements = scanner.nextLine().trim().split("=");
+            if (elements.length == 2) {
+                items.add(new Item(elements[0], Integer.parseInt(elements[1])));
+            }
+
+        }
+
+        return items;
     }
 
     public void loadU1(ArrayList<Item> items) {
-
+        
     }
 
     public void loadU2(ArrayList<Item> items) {
@@ -15,7 +31,7 @@ public class Simulation {
     }
 
     public double runSimulation(ArrayList<Rocket> rockets) {
-        
+
         return 0;
     }
 }
